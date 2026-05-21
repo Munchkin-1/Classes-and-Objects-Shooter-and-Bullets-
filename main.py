4253f2fe3d8f3f310779c8b6b61c985e57c6664b
Phoenix <trumanparks@gmail.com>
2026-05-21 15:30:32 -0500

    finished

--- main.py
+++ main.py
@@ -32,6 +32,10 @@ class Player(Turtle):
         self.st()
         screen.onkeypress(self.turn_left, left_key)
         screen.onkeypress(self.turn_right, right_key)
+        
+    def fire(self):
+        
+        self.bullets.append(Bullet(self))
 
     def turn_left(self):
         self.left(10)
@@ -41,26 +45,67 @@ class Player(Turtle):
 
     def move(self):
         self.forward(4)
+        
         if self.xcor() > 230 or self.xcor() < -230:
             self.setheading(180 - self.heading())
         if self.ycor() > 230 or self.ycor() < -230:
             self.setheading(-self.heading())
 
+class Bullet(Turtle):
+    def __init__(self, player):
+        super().__init__()
+        self.ht()
+        self.speed(0)
+        
+        self.color(player.pencolor()) 
+        self.penup()
+        self.goto(player.xcor(), player.ycor())
+        self.setheading(player.heading())
+        self.shape("triangle")
+        self.forward(15) 
+        self.player = player
+        self.st()
+
+    def move(self):
+        self.forward(10)
+        if self.xcor() > 230 or self.xcor() < -230 or self.ycor() > 230 or self.ycor() < -230:
+            self.remove()
+
+    def remove(self):
+        if self in self.player.bullets:
+            self.ht()
+            self.player.bullets.remove(self)
+
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(520,520)
-# Key Binding. Connects key presses and mouse clicks with function calls
-screen.listen()
 
+screen.listen()
 
 playing_area()
 
-p1 = Player(-100, 0, "red",screen, "d", "a")
-p2 = Player(100,0,"blue",screen, "Right","Left")
+p1 = Player(-100, 0, "red", screen, "d", "a")
+p2 = Player(100, 0, "blue", screen, "Right", "Left")
+
+screen.onkeypress(p1.fire, "w")
+screen.onkeypress(p2.fire, "Up")
+
 
 while p1.alive and p2.alive:
     p1.move()
     p2.move()
-
+    
+    for b in p1.bullets:
+        b.move()
+        if b.distance(p2) < 20:   
+            p2.alive = False      
+            b.remove()
+            
+    for b in p2.bullets:
+        b.move()
+        if b.distance(p1) < 20:   
+            p1.alive = False      
+            b.remove()
 
 screen.exitonclick()
